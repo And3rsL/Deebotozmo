aa73f8689a86fe08d0e598a2f33e23e5083f5162
Robert Resch <robert@resch.dev>
2021-09-20 21:23:36 +0200

    refactor mqtt

--- deebotozmo/ecovacs_mqtt.py
+++ deebotozmo/ecovacs_mqtt.py
@@ -3,6 +3,7 @@ import json
 import logging
 import ssl
 from typing import Dict, MutableMapping
+from typing import MutableMapping, Optional
 
 from gmqtt import Client
 from gmqtt.mqtt.constants import MQTTv311
@@ -17,18 +18,21 @@ def _get_topic(vacuum: Vacuum) -> str:
     return f"iot/atr/+/{vacuum.did}/{vacuum.get_class}/{vacuum.resource}/+"
 
 
+class NotInitializedError(Exception):
+    pass
+
+
 class EcovacsMqtt:
     """Handle mqtt connections."""
 
-    def __init__(self, auth: RequestAuth, *, continent: str, country: str):
+    def __init__(self, *, continent: str, country: str):
         self._subscribers: MutableMapping[str, VacuumBot] = {}
         self._port = 443
         self._hostname = f"mq-{continent}.ecouser.net"
         if country.lower() == "cn":
             self._hostname = "mq.ecouser.net"
 
-        client_id = f"{auth.user_id}@ecouser/{auth.resource}"
-        self._client = Client(client_id)
+        self._client: Optional[Client] = None
 
         # pylint: disable=unused-argument
         async def _on_message(
@@ -55,14 +59,21 @@ class EcovacsMqtt:
                 if bot:
                     data = json.loads(payload)
                     await bot.handle(topic_split[2], data, False)
-            except Exception as ex:  # pylint: disable=broad-except
-                _LOGGER.error(ex, exc_info=True)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.error("An exception occurred", exc_info=True)
 
-        self._client.on_message = _on_message
+        self.__on_message = _on_message
+
+    async def initialize(self, auth: RequestAuth)-> None:
+        """Initialize MQTT."""
+        if self._client is not None:
+            self.disconnect()
+
+        client_id = f"{auth.user_id}@ecouser/{auth.resource}"
+        self._client = Client(client_id)
+        self._client.on_message = self.__on_message
         self._client.set_auth_credentials(auth.user_id, auth.token)
 
-    async def initialize(self) -> None:
-        """Initialize MQTT."""
         ssl_ctx = ssl.create_default_context()
         ssl_ctx.check_hostname = False
         ssl_ctx.verify_mode = ssl.CERT_NONE
@@ -72,8 +83,8 @@ class EcovacsMqtt:
 
     async def subscribe(self, vacuum_bot: VacuumBot) -> None:
         """Subscribe for messages for given vacuum."""
-        if not self._client.is_connected:
-            await self.initialize()
+        if self._client is None:
+            raise NotInitializedError
 
         vacuum = vacuum_bot.vacuum
         self._client.subscribe(_get_topic(vacuum))
