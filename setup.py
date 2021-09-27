c100f92e012cbe73ce55bb9c7464480d4a796dbd
Robert Resch <robert@resch.dev>
2021-09-27 21:20:37 +0200

    Revert "Add CLI back (#55)"
    This reverts commit fd7a83b0

--- setup.py
+++ setup.py
@@ -33,9 +33,4 @@ setup(
     packages=["deebotozmo"],
     package_data={"deebotozmo": ["py.typed"]},
     install_requires=list(val.strip() for val in open("requirements.txt")),
-    entry_points={
-        "console_scripts": [
-            "deebotozmo=deebotozmo.cli:cli",
-        ],
-    },
 )
